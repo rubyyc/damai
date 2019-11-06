bfdffa7d1c6079cfbb1ed1467f3cb21e60225e79
yuanchang <759337828@qq.com>
2019-11-06 10:55:47 +0000

    select UI

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
 
  validates_presence_of :name
 
+ STATUS = ["draft", "public", "private"]
+ validates_inclusion_of :status, :in => STATUS
+
  def to_param
   "#{self.id}-#{self.name}"
  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191106103735) do
+ActiveRecord::Schema.define(version: 20191106105104) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "status",      default: "draft"
   end
 
   create_table "users", force: :cascade do |t|
