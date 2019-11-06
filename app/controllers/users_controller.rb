57700d4129525d82eef74385a70fc8793d6c4043
yuanchang <759337828@qq.com>
2019-11-06 11:53:01 +0000

    user profile show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+   before_action :find_user
+
+   def show
+   end
+
 
   def edit
     @user = current_user
@@ -21,6 +26,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:time_zone)
+  end
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
   end
 
 end
