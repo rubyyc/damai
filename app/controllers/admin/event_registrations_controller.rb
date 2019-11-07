c3799085915c19a32a1fb6142bd7c2c487b3bc3f
yuanchang <759337828@qq.com>
2019-11-07 09:17:40 +0000

    多项筛选

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -5,7 +5,36 @@
      def index
          # @registrations = @event.registrations.includes(:ticket).order("id DESC")
          @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(20)
-       end
+
+         # if params[:status].present? && Registration::STATUS.include?(params[:status])
+         #   @registrations = @registrations.by_status(params[:status])
+         # end
+         #
+         # if params[:ticket_id].present?
+         #   @registrations = @registrations.by_ticket(params[:ticket_id])
+         # end
+
+         if Array(params[:statuses]).any?
+           @registrations = @registrations.by_status(params[:statuses])
+         end
+
+         if Array(params[:ticket_ids]).any?
+           @registrations = @registrations.by_ticket(params[:ticket_ids])
+         end
+
+         if params[:start_on].present?
+           @registrations = @registrations.where( "created_at >= ?", Date.parse(params[:start_on]).beginning_of_day )
+         end
+
+         if params[:end_on].present?
+           @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+         end
+
+         if params[:registration_id].present?
+           @registrations = @registrations.where( :id => params[:registration_id].split(",") )
+         end
+
+     end
 
      def destroy
          @registration = @event.registrations.find_by_uuid(params[:id])
