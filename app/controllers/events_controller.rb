0f91a1d3e9947efa824b21fbfe5df05c3531f749
yuanchang <759337828@qq.com>
2019-11-07 09:26:08 +0000

    status pulic private 私人访问 公开访问

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,14 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.rank(:row_order).all
+    # @events = Event.rank(:row_order).all
+    @events = Event.only_public.rank(:row_order).all
   end
 
   def show
-    @event = Event.find(params[:id])
+    # @event = Event.find(params[:id])
+    @event = Event.only_available.find(params[:id])
+
   end
 
 end
